0a74990f863b5ae8d7e08852d32072c2fc96f8f6
omsl <94546656+tl-ohm@users.noreply.github.com>
2025-05-12 18:32:01 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, redirect, url_for, request, jsonify, abort
+import requests
 
 app = Flask(__name__, template_folder="client/templates", static_folder="client/static")
 
@@ -34,7 +35,27 @@ def ansi():
     if request.method == "POST":
         return 'fuck you'
     
-    return render_template('ansi.html', image=url_for('static', filename='images/image.png'))
+    return render_template('ansi.html')
+
+@app.route('/email', methods=['POST', 'GET'])
+def email():
+
+    maintenance_message = check_maintenance()
+    
+    if maintenance_message:
+        return maintenance_message
+
+    if request.method == "POST":
+        
+        DATA = {
+            'content': f"""Requester: {request.form.get('email')}\nWants: {request.form.get('suc')}\nWants To Pay: {request.form.get('bid')}"""
+        }
+        DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1371613773986529340/sFeR8_iVLvfqmPJCQzV2DfeB4vT_uQPGU3-fXi4g3KoGqB5j1MDFo8jaeR5Gv-KcQf50"
+        requests.post(DISCORD_WEBHOOK_URL, data=DATA)
+        return render_template("messagepage.html", message="Sent request succesfully. We will get back to you as soon as possible.")
+    
+    return render_template('email.html')
+
 
 @app.route('/protected')
 def Protected():
