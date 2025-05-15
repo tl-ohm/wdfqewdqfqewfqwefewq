1abb6bf59f4f1f5193a9ef17fe0d89f1b9c9454c
omsl <94546656+tl-ohm@users.noreply.github.com>
2025-05-15 07:06:44 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-
-from flask import Flask, render_template, redirect, url_for, request, jsonify, abort
-import requests
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__, template_folder="client/templates", static_folder="client/static")
 app.config['SERVER_NAME'] = '.olyium.com'
@@ -9,70 +7,43 @@ maintenance_mode = False
 
 def check_maintenance():
     if maintenance_mode:
-        return 'olyium is 100% down rn. (jokeing ts is still up)'
+        return 'olyium is under maintenance.'
     return None
 
 @app.route('/', subdomain='games')
 def dqwdq_index():
     return render_template('games_landingpage.html')
-    
+
 @app.route('/', methods=['POST', 'GET'])
 def LandingPage():
-
     maintenance_message = check_maintenance()
-    
     if maintenance_message:
         return maintenance_message
-
-    if request.method == "POST":
-        return 'fuck you'
-    
     return render_template('landingpage.html', image=url_for('static', filename='images/image.png'))
 
 @app.route('/ansi', methods=['POST', 'GET'])
 def ansi():
-
     maintenance_message = check_maintenance()
-    
     if maintenance_message:
         return maintenance_message
-
-    if request.method == "POST":
-        return 'fuck you'
-    
     return render_template('ansi.html')
 
 @app.route('/mines', methods=['POST', 'GET'])
 def mines():
-
     maintenance_message = check_maintenance()
-    
     if maintenance_message:
         return maintenance_message
-
-    if request.method == "POST":
-        
-        return 'fuck you nigga'
-        
     return render_template('mines.html')
-
 
 @app.route('/protected')
 def Protected():
-
-    try:
-
-        Referer = request.headers.get("Referer")
-    
-    except:
-
-        pass
-
-    return f'You have been redirected to olyiums protected network to ensure the stability and uptime of {Referer}'
+    referer = request.headers.get("Referer")
+    return f'You have been redirected to Olyium\'s protected network to ensure the stability and uptime of {referer}'
 
 @app.errorhandler(Exception)
 def handle_all_errors(e):
-    return redirect(url_for('LandingPage'))
+    print(f"[ERROR] {e}")
+    return "An unexpected error occurred.", 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0') 
+    app.run(debug=True, host='0.0.0.0')
